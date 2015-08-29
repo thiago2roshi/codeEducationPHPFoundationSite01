3b52deec95f4d3511733af6180b87eb8cbdf20a7
Thiago'roshi' Souza <mutenroshi930@gmail.com>
2015-08-29 20:25:29 -0300

    criando paginas simples - nada complexo, "contato.php" feito com form

---
+++ contato.php
@@ -0,0 +1,9 @@
+<h1>Contato</h1><br>
+Para falar conosco, so preencher o formulario, mas não vou dar certeza de resposta
+<br>
+<form class="" action="?arquivo=enviado.php" method="post">
+    <input name="nome"      type="text"   placeholder="Maria Jose da Silva"><br>
+    <input name="email"     type="email"  placeholder="user@email.com"><br>
+    <input name="descricao" type="text"   placeholder="Em que posso ajudar?" height="60"><br>
+    <input name="enviar"    type="submit" value="enviar">
+</form>
---
+++ empresa.php
@@ -0,0 +1,4 @@
+<h1>Empresa</h1>
+<p>
+    Essa é uma Empresa ficticia e tudo exibido é falso, mas aceito o seu dinheiro sem problemas
+</p>
