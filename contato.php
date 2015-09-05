9b2aff4eecb25251c477c90256e41e91d84d94dd
Thiago'roshi' Souza <mutenroshi930@gmail.com>
2015-09-05 16:33:33 -0300

    contato.php => reformulando design; contato_send=> resultado do contato.php

--- contato.php
+++ contato.php
@@ -1,9 +1,17 @@
 <h1>Contato</h1><br>
-Para falar conosco, so preencher o formulario, mas não vou dar certeza de resposta
+Para falar conosco, so preencher o formulario, mas não vou dar certeza de resposta <br>
 <br>
-<form class="" action="?arquivo=enviado.php" method="post">
-    <input name="nome"      type="text"   placeholder="Maria Jose da Silva"><br>
-    <input name="email"     type="email"  placeholder="user@email.com"><br>
-    <input name="descricao" type="text"   placeholder="Em que posso ajudar?" height="60"><br>
-    <input name="enviar"    type="submit" value="enviar">
+<form class="form" action="contato_send" method="post">
+    <p>
+        <label>Nome: </label>
+        <input name="nome"      type="text"   placeholder="Maria Jose da Silva">
+    </p><p>
+        <label>Email: </label>
+        <input name="email"     type="email"  placeholder="user@email.com">
+    </p><p>
+        <label>Descricao: </label>
+        <input name="descricao" type="text"   placeholder="Em que posso ajudar?">
+    </p><p>
+        <input name="enviar"    type="submit" value="enviar">
+    </p>
 </form>
---
+++ contato_send.php
@@ -0,0 +1,21 @@
+<?php
+$nome      = $_POST['nome'];
+$email     = $_POST['email'];
+$descricao = $_POST['descricao'];
+
+echo '<h1>Dados enviados com sucesso, abaixo seguem os dados que você enviou</h1><br>';
+
+echo '
+    <p>
+        <label>Nome: </label>
+        <input name="nome"      type="text"   value=' . $nome . '>
+    </p><p>
+        <label>Email: </label>
+        <input name="email"     type="email"  value=' . $email . '>
+    </p><p>
+        <label>Descricao: </label>
+        <input name="descricao" type="text"   value=' . $descricao . '>
+    </p>
+
+
+?>
