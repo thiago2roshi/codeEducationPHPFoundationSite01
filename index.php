2e0898dc63d430224f32bf27fab544bb77bed2c2
Thiago'roshi' Souza <mutenroshi930@gmail.com>
2015-09-05 16:38:56 -0300

    index.php => adaptando o uso da classe "Router"

--- index.php
+++ index.php
@@ -1,17 +1,9 @@
 <?php
 require_once 'top.php';
+require_once 'Route.php';
 
-$route = parse_url('http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
-$path  = explode('/', $route['path']); // var_dump array(2) { [0]=> string(0) "" [1]=> string(4) "home" }
-
-if (file_exists($path[1] . '.php')) //error_handler para o _GET, para parametro invalido
-{
-    require_once($path[1] . '.php');
-}
-else
-{
-    require_once 'ops.php';
-}
+$rota = new Route;
+$path = $rota->validateRoute();
 
 require_once 'bottom.php';
 
