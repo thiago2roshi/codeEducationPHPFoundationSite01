23040c56b3f20dece05a2298bb07d67d11dbaa59
Thiago'roshi' Souza <mutenroshi930@gmail.com>
2015-09-27 16:49:47 -0300

    corrigindo falha de query sql na linha 22 (create table)

--- app/unitTest/fixture.php
+++ app/unitTest/fixture.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once '../SON/Conexao.php';
-require_once '../config.php';
+require_once("../SON/Conexao.php");
+require_once("../config.php");
 
 echo "
 /**********************
@@ -9,18 +9,17 @@ echo "
  **********************/
 ";
 
-$db = \SON\Conexao($database['sgdb'], $database['host'], $database['db'], $database['user'], $database['pass']);
+$db = new \SON\Conexao($database['sgdb'], $database['host'], $database['db'], $database['user'], $database['pass']);
+$conn = $db->connect();
 
 echo "Dropando TABLE";
-
-$conn = $db->connect();
 $conn->query("DROP TABLE if exist teste;");
 
 echo " - OK\n";
 echo "Criando TABLE";
 
 $conn->query(
-                "CREATE TABLE if not exist teste
+                "CREATE TABLE if not exists teste
                 (
                     id INT auto_increment,
                     text VARCHAR(10),
@@ -34,7 +33,7 @@ for ($i=0; $i < 10 ; $i++) {
     $text = "text{$i}";
 
     $stm = $conn->prepare("INSERT INTO teste (text) VALUES (:text);");
-    $stm = bindParam(":text", $text);
+    $stm->bindParam(":text", $text);
     $stm->execute();
 }
 echo " - OK\n";
