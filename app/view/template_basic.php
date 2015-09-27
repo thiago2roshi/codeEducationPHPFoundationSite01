39f06b68b6de53fc77bfeefbb3c6fc5f88dffb03
Thiago'roshi' Souza <mutenroshi930@gmail.com>
2015-09-27 16:10:08 -0300

    criando templates para as paginas serem carregadas via DataBase

---
+++ app/view/template_basic.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @param $title    titulo da pagina
+ * @param $content  conteudo da pagina
+ */
+
+echo "
+    <h1>{$title}</h1>
+    <div>
+        <p>{$content}</p>
+    </div>
+";
+
+ ?>
