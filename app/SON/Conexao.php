642df39d101b6aaa92ff1341212c573f76bc5cbc
Thiago'roshi' Souza <mutenroshi930@gmail.com>
2015-09-27 15:28:31 -0300

    trazendo tratativas de connect.php para a classe _Conexão_, tornando a mais independente

--- app/SON/Conexao.php
+++ app/SON/Conexao.php
@@ -12,33 +12,33 @@ class Conexao implements ConexaoInterface
     private $sgdb;
 	private $host;
 	private $db;
-	private $user;
+    private $user;
 	private $pass;
 
-    /**
-     * construtor da Conexao
-     * @param [text] $sgdb servidor::MySql, MsSql, Oracle
-     * @param [text] $host Endereco do servidor
-     * @param [text] $db   nome do banco do dados
-     * @param [text] $user login do banco de daos
-     * @param [text] $pass senha do banco de dados
-     */
-	public function __construct($sgdb, $host, $db, $user, $pass)
-	{
-        $this->sgdb = $sgdb;
-		$this->host = $host;
-		$this->db   = $db;
-		$this->user = $user;
-		$this->pass = $pass;
-	}
 
     /**
      * statement da conexao com o bando de dados
      * @return objeto conexao com o bando
      */
+    public function __construct($sgdb, $host, $db, $user, $pass)
+    {
+        $this->sgdb = $sgdb;
+        $this->host = $host;
+        $this->db   = $db;
+        $this->user = $user;
+        $this->pass = $pass;
+    }
 	public function connect()
     {
-        return new \PDO("{$this->sgdb}:host={$this->host};dbname={$this->db}", $this->user, $this->pass);
+        try
+        {
+            return new \PDO("{$this->sgdb}:host={$this->host};dbname={$this->db}", $this->user, $this->pass);
+        }
+        catch (Exception $e)
+        {
+            die('Erro Codigo: ' . $e->getCode() . ': ' . $e->getMessage() . '\n' . $e->getTraceAsString() . '\n');
+        }
+
     }
 
 }
