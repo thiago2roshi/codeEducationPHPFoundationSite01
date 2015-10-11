8eced5ccc0574b07a8eae45b27583ed0ec0b8faa
Thiago'roshi' Souza <mutenroshi930@gmail.com>
2015-10-11 17:08:08 -0300

    reestruturando mais uma vez a organização do arquivo

---
+++ SON/ConexaoInterface.class.php
@@ -0,0 +1,13 @@
+<?php
+
+//evitar bugs de funcao não implementada, alem de deixar o codigo mais limpo
+/**
+ * @author Thiago Souza Santos aka. ThiagoRoshi <ads.thiagosouza@gmail.com>
+ */
+
+namespace SON;
+
+interface ConexaoInterface
+{
+	public function connect();
+}
