cfccd6a5a259bb29fc7553b7e249de85fb0de44d
Thiago'roshi' Souza <mutenroshi930@gmail.com>
2015-10-11 17:11:54 -0300

    reformulando index para autoload

--- index.php
+++ index.php
@@ -1,12 +1,11 @@
 <?php
-require_once('view/top.php');
+// require_once('view/top.php');
 // require_once('../app/Route.php');
-require 'vendor/autoload.php';
-
-// $loader = require __DIR__ . 'vendor/autoload.php';
-// $loader->add('SON', 'SON//');
-
-$rota = new \SON\Route;
+require_once(__DIR__ . '/vendor/autoload.php');
+// $loader = require __DIR__ . '/vendor/autoload.php';
+// $loader->add()
+use \SON\Route;
+$rota = new \SON\Route();
 $path = $rota->validateRoute();
 
 require_once('view/bottom.php');
