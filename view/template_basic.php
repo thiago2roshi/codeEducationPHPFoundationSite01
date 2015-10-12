c5fac47bcf177be78633b6236acb8fd6d873cf9f
Thiago'roshi' Souza <mutenroshi930@gmail.com>
2015-10-12 01:11:10 -0300

    ajustando templates

--- view/template_basic.php
+++ view/template_basic.php
@@ -1,14 +1,9 @@
 <?php
-/**
- * @param $title    titulo da pagina
- * @param $content  conteudo da pagina
- */
-public function templateBasic($title, $content)
+
+function templateBasic($title, $content)
 {
     echo "
         <h1>{$title}</h1>
-        <div>
-            <p>{$content}</p>
-        </div>
+        <p>{$content}</p>
     ";
 }
--- view/template_form.php
+++ view/template_form.php
@@ -8,23 +8,26 @@
  * @param $content : conteudo da pagina
  * @param $form_action : action do formulario
  */
+function templateForm()
+{
+    echo "
+        <h1>{$title}</h1>
+        <p>{$content}</p>
 
-echo "
-    <h1>{$title}</h1>
-    <p>{$content}</p>
+        <form class='form' action='{$form_action}' method='post'>
+            <p>
+                <label class = 'textfield'>Nome: </label>
+                <input class = 'input-xlarge' name = 'nome' type = 'text' placeholder = 'Maria Jose da Silva'>
+            </p><p>
+                <label class = 'textfield'>Email: </label>
+                <input class = 'input-xlarge' name = 'email' type = 'email' placeholder = 'user@email.com'>
+            </p><p>
+                <label class = 'textfield'>Descricao: </label>
+                <input class = 'input-xlarge' name = 'descricao' type = 'text' placeholder  = 'Em que posso ajudar?'>
+            </p><p>
+                <input class = 'btn btn-block' name = 'enviar' type = 'submit' value = 'enviar'>
+            </p>
+        </form>
+    ";
 
-<form class='form' action='{$form_action}' method='post'>
-    <p>
-        <label class = 'textfield'>Nome: </label>
-        <input class = 'input-xlarge' name = 'nome' type = 'text' placeholder = 'Maria Jose da Silva'>
-    </p><p>
-        <label class = 'textfield'>Email: </label>
-        <input class = 'input-xlarge' name = 'email' type = 'email' placeholder = 'user@email.com'>
-    </p><p>
-        <label class = 'textfield'>Descricao: </label>
-        <input class = 'input-xlarge' name = 'descricao' type = 'text' placeholder  = 'Em que posso ajudar?'>
-    </p><p>
-        <input class = 'btn btn-block' name = 'enviar' type = 'submit' value = 'enviar'>
-    </p>
-</form>
-";
+}
