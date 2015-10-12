e81d0aab86c6960f30639ebe4f9d6ada1651653c
Thiago'roshi' Souza <mutenroshi930@gmail.com>
2015-10-12 01:08:07 -0300

    transferidos dados para o banco de dados

--- view/servicos.php
+++
@@ -1,6 +0,0 @@
-<h1>Serviços</h1>
-<p>
-    Prestamos todos os tipos de serviço, de limpeza de caixa d'agua à manutenção
-    do seu servidor, com direito á elefante psiquico de guerra com duas cabeças,
-    ou para os mais simplistas, uma portal gun para o seu dia a dia.
-</p>
