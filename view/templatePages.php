b7388a433336204572750aa28020951d6326dc13
Thiago'roshi' Souza <mutenroshi930@gmail.com>
2015-10-13 21:11:44 -0300

    criando arquivo unico para templates

---
+++ view/templatePages.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * @author Thiago Souza <ads.thiagosouza@gmail.com>
+ *
+ * @description template de paginas
+ *
+ * @param $title   : titulo da pagina
+ * @param $content : conteudo da pagina
+ * @param $form_action : action do formulario
+ */
+
+ function templateSeach($title, $content)
+ {
+     foreach($db->search)//TERMINA SAPOHA
+     echo "
+         <div id='search-block' class=''>
+             <div id='search-container' class=''>
+                 <label>{$title}</label>
+                 <div id='' class=''>{$content}</div>
+             </div>
+         </div>
+         ";
+ }
+
+function templateForm($title, $content, $form_action)
+{
+    echo "
+        <h1>{$title}</h1>
+        <p>{$content}</p>
+
+        <form class='form' action='{$form_action}' method='post'>
+            <p>
+                <label class = 'textfield'>Nome: </label>
+                <input class = 'input-xlarge' name = 'nome' type = 'text' placeholder = 'Maria Jose da Silva'>
+            </p><p>
+                <label class = 'textfield'>Email: </label>
+                <input class = 'input-xlarge' name = 'email' type = 'email' placeholder = 'user@email.com'>
+            </p><p>
+                <label class = 'textfield'>Descricao: </label>
+                <input class = 'input-xlarge' name = 'descricao' type = 'text' placeholder  = 'Em que posso ajudar?'>
+            </p><p>
+                <input class = 'btn btn-block' name = 'enviar' type = 'submit' value = 'enviar'>
+            </p>
+        </form>
+    ";
+
+}
