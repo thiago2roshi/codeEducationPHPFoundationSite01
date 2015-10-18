66e6b2aeff7a29dea64a94a3dc83fc9a3e16e121
Thiago'roshi' Souza <mutenroshi930@gmail.com>
2015-10-17 21:39:20 -0300

    ajustando arquivos para utilizar o bootstrap via CDN

--- view/top.php
+++ view/top.php
@@ -1,35 +1,55 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <meta charset ="utf-8">
-        <title>CodeEducation PhpFoundation01</title>
-        <meta content="@thiagoroshi" name="author">
-        <link rel="stylesheet" href="./css/bootstrap.min.css"/>
-        <link rel="stylesheet" href="./css/bootstrap-responsive.min.css">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <style>
-            body{padding-top: 60px;};
-        </style>
-        <link rel="shortcut icon" href="./favicon.ico">
-    </head>
+<html lang="pt-BR">
+<head>
+    <meta charset ="utf-8">
+    <title>CodeEducation PhpFoundation01</title>
+    <meta content="@thiagoroshi" name="author">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+    <style>
+    body{padding-top: 60px;};
+    </style>
+    <link rel="shortcut icon" href="./favicon.ico">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
 <body>
-<div class="navbar navbar-inverse navbar-fixed-top">
-    <div class="navbar-inner">
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+        <!-- <div class="container container-fluid"> -->
         <div class="container-fluid">
-            <a class='btn btn-navbar' data-target='.nav-collapse' data-toggle='collapse'>
-                <span class="icon-bar"> </span>
-            </a>
-            <a class="brand" href="#">CodeEducation PhpFoundation 1.0</a>
-            <div class="nav nav-bar nav-collapse collapse">
-                <ul class='nav'>
-                    <li><a href ="home">home</a></li>
-                    <li><a href ="empresa">Empresa</a></li>
-                    <li><a href ="produtos">Produtos</a></li>
-                    <li><a href ="servicos">Serviços</a></li>
-                    <li><a href ="contato">contato</a></li>
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                    <span class="sr-only">Toggle Nav</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">CodeEducation PhpFoundation 1.0</a>
+            </div><!--./navbar-header-->
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div id="navbar" class="collapse navbar-collapse">
+                <ul class='nav navbar-nav'>
+                    <li><a href="home">home</a></li>
+                    <li><a href="empresa">Empresa</a></li>
+                    <li><a href="produtos">Produtos</a></li>
+                    <li><a href="servicos">Serviços</a></li>
+                    <li><a href="contato">contato</a></li>
                 </ul>
-            </div><!--./nav-collase-->
+                <form class="navbar-form navbar-right" role="search">
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Pesquisar...">
+                    </div>
+                    <button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-search"></span></button>
+                </form>
+            </div>
         </div>
-    </div>
-</div>
-<div class="container article">
+    </nav>
+    <div class="container article">
