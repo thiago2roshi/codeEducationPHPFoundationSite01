35a8eca39095f408a53f3271b19b4aebcc97a6e1
Thiago'roshi' Souza <mutenroshi930@gmail.com>
2015-11-07 21:00:52 -0200

    index.php -> removido dependencias da classe Route           -> adicionado Twig e logica de criação de paginas models/Pages.model.php -> corrigido $PATH para estetica de codigo ~~tenho que parar de frescura~~ composerUpdate.sh -> script para gerenciar o composer sem acessar o container docker

--- composerUpdate.sh
+++ composerUpdate.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
+echo "============================="
+echo "| Iniciando Composer Update |"
+echo "============================="
+cd /var/www
+echo "=========================="
+echo "|| Atualizando Composer ||"
+echo "=========================="
 composer update
+echo "============================"
+echo "|| Composer update pronto ||"
+echo "||  bora programar carai  ||"
+echo "============================"
+
--- index.php
+++ index.php
@@ -5,22 +5,47 @@
  */
 require __DIR__ . '/vendor/autoload.php';
 
-use app\Route;
-use app\TemplatePage;
+use app\Pages;
 
-require_once __DIR__ .'/view/top.php';
-require_once __DIR__ .'/view/templatePages.php';
 
-$rota = new Route();
-$template = new TemplatePage();
-
-$page = $rota->validateRoute();
-
-if ($page !== null) {
-    print($template->templateBasic($page));
-}else {
-    include 'view/404.html';
+if (substr($_SERVER['REQUEST_URI'], 1) !== false) {
+    $rota = substr($_SERVER['REQUEST_URI'], 1);
+} else {
+    $rota = "home";
 }
 
+$page = new Pages();
+$rs = $page->verPagina($rota);
 
-require_once('view/bottom.php');
+
+$config = array(
+    'twig' => array(
+        'cache'       => __DIR__ . '/cache', // cache das paginas geradas
+        'filesystem'  => __DIR__ . '/view',  // onde os templates da pagina ficam
+        'auto_reload' => 'true'               // gerar novas paginas sempre que o template mude
+    )
+);
+
+$loader = new Twig_Loader_Filesystem($config['twig']['filesystem']);
+$twig   = new Twig_Environment($loader);
+// $twig   = new Twig_Environment(
+//                                 $loader, array(
+//                                     'cache'       => __DIR__ . '/cache',
+//                                     'auto_reload' => 'true'
+//                                     )
+//                                 );
+
+echo $twig->render($rs[0]->getType().'.twig', array(
+                                "title"   => $rs[0]->getTitle(),
+                                "content" => $rs[0]->getContent()
+                            )
+                    );
+
+// if ($page !== null) {
+//     print($template->templateBasic($page));
+// }else {
+//     include 'view/404.html';
+// }
+//
+//
+// require_once('view/bottom.php');
