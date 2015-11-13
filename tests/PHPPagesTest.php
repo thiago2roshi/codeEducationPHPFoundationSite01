0c60e6de1fe39246f78c73ce7b2b7acbe0eab460
Thiago Souza 'Roshi <mutenroshi930@gmail.com>
2015-11-13 13:26:00 -0200

    Create PHPPagesTest.php

---
+++ tests/PHPPagesTest.php
@@ -0,0 +1,31 @@
+<?php
+
+use PHPUnit_Framework_TestCase as PHPUnit;
+use \app\Pages as Pages;
+
+class PHPNativeElementsTest extends PHPUnit
+{
+
+    protect $Pages;
+
+    public function setUp()
+    {
+        /** parametros iniciais
+         *   Executados no inicio 
+         *    do teste unitario 
+         **/
+        $this->page = new Pages();
+    }
+    
+    public function testPagesClass()
+    {
+        $this->page->verPagina('home');
+        
+    }
+    public function tearDown()
+    {
+        /* parametros de encerramento*/
+    }    
+}
+
+?>
